c82c67e2c28827300095bf0e2e47cca213aa3a3f
tcooper98 <97857190+tcooper98@users.noreply.github.com>
2023-06-02 10:51:41 -0400

    stepper working

--- client/src/components/history.js
+++ client/src/components/history.js
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
-import Stepper from '@mui/material/Stepper'
-import Step  from '@mui/material/Step';
+import React, { useState } from 'react';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import { Card, StepButton } from '@mui/material';
+import { StepButton } from '@mui/material';
 
 const steps = ['Step 1', 'Step 2', 'Step 3'];
 
 export const History = () => {
-  const [activeStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(0);
 
-  const setActiveStep = (index) => {
+  const handleStepClick = (index) => {
+    setActiveStep(index);
+  };
 
-    console.log(index);
-   }
+  const renderStepContent = (step) => {
+    switch (step) {
+      case 0:
+        return <div>Step 1 Content</div>;
+      case 1:
+        return <div>Step 2 Content</div>;
+      case 2:
+        return <div>Step 3 Content</div>;
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
-   
-    <Stepper nonLinear activeStep={activeStep}>
-      {steps.map((step, index) => (
-        <Step key={step}>
-        <StepButton onClick={() => setActiveStep({index})}>
-        {step}
-        </StepButton>
-        </Step>
+    {renderStepContent(activeStep)}
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((step, index) => (
+          <Step key={step}>
+            <StepButton onClick={() => handleStepClick(index)}>
+              {step}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
 
-      ))}
-    </Stepper>
-    
+      
     </>
-  )
-}
+  );
+};
